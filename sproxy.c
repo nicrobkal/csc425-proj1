8028308ebebf409b7f607fe05d548786cfbbb96d
nicrobkal <nicrobkal@gmail.com>
2020-02-24 07:57:54 -0700

    Reimplemented Server socket accepting

--- sproxy.c
+++ sproxy.c
@@ -96,75 +96,108 @@ int main(int argc, char *argv[])
     telnetAddr.sin_addr.s_addr = INADDR_ANY; 
     telnetAddr.sin_port = htons(23);
 
-    //Accept the client
-    if ((serverSock = accept(telnetSock, (struct sockaddr *)&telnetAddr, (socklen_t*)&telnetAddrLen))<0) 
+    //Create socket file descriptor
+    if ((serverSock = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
-        fprintf(stderr, "Accept failed. Terminating.\n");
+        fprintf(stderr, "Socket failed to connect. Terminating.\n");
+        return 1;
+    } 
+       
+    //Attach socket to port
+    if (setsockopt(serverSock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+    { 
+        fprintf(stderr, "Failed setting sock options. Terminating.\n");
+    	return 1;
+    }
+
+    serverAddr.sin_family = AF_INET; 
+    serverAddr.sin_addr.s_addr = INADDR_ANY; 
+    serverAddr.sin_port = htons(atoi(argv[1])); 
+       
+    //Bind ip to socket
+    if(bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) 
+    {
+        fprintf(stderr, "Server binding failed. Terminating.\n");
+        return 1;
+    }
+    
+    //Enable listening on given socket
+    if (listen(serverSock, 3) < 0) 
+    { 
+        fprintf(stderr, "Listening failed. Terminating.\n"); 
+        exit(EXIT_FAILURE); 
+    }
+
+    //Accept the client
+    if ((serverSock = accept(serverSock, (struct sockaddr *)&serverAddr, (socklen_t*)&serverAddrLen))<0) 
+    { 
+        fprintf(stderr, "Server accept failed. Terminating.\n");
         return 1;
     }
 
-    //While user is still inputting data
-    while(1)
-    {
-        //Clear the set
-        int n = 0;
-        FD_ZERO(&readfds);
-
-        //Add descriptors
-        FD_SET(telnetSock, &readfds);
-        FD_SET(serverSock, &readfds);
-
-        //Find larger file descriptor
-        if(telnetSock > serverSock)
+        //While user is still inputting data
+        while(1)
         {
-            n = telnetSock + 1;
-        }
-        else
-        {
-            n = serverSock + 1;
-        }
+            //Clear the set
+            int n = 0;
+            FD_ZERO(&readfds);
 
-        //Select returns one of the sockets or timeout
-        int rv = select(n, &readfds, NULL, NULL, NULL);
+            //Add descriptors
+            FD_SET(telnetSock, &readfds);
+            FD_SET(serverSock, &readfds);
 
-        if (rv == -1)
-        {
-            fprintf(stderr, "Select() function failed.\n");
-	    close(telnetSock);
-	    close(serverSock);
- 	    return 1;
-        }
-        else if(rv == 0)
-        {
-            printf("Timeout occurred! No data after 10.5 seconds.\n");
+            //Find larger file descriptor
+            if(telnetSock > serverSock)
+            {
+                n = telnetSock + 1;
+            }
+            else
+            {
+                n = serverSock + 1;
+            }
+
+            //Select returns one of the sockets or timeout
+            int rv = select(n, &readfds, NULL, NULL, NULL);
+
+            if (rv == -1)
+            {
+                fprintf(stderr, "Select() function failed.\n");
             close(telnetSock);
-	    close(serverSock);
-	    return 1;
-        }
-        else
-        {
-            //One or both descrptors have data
-            if(FD_ISSET(telnetSock, &readfds))
-            {
-                recv(telnetSock, telnetBuff, maxLen, 0);
-                send(serverSock, telnetBuff, strlen(telnetBuff), 0);
-		printf("%s", telnetBuff);
+            close(serverSock);
+            return 1;
             }
-            if(FD_ISSET(serverSock, &readfds))
+            else if(rv == 0)
             {
-                recv(serverSock, serverBuff, maxLen, 0);
-                send(telnetSock, serverBuff, strlen(serverBuff), 0);
-		printf("%s", serverBuff);
+                printf("Timeout occurred! No data after 10.5 seconds.\n");
+                close(telnetSock);
+            close(serverSock);
+            return 1;
             }
-        }
+            else
+            {
+                //One or both descrptors have data
+                if(FD_ISSET(telnetSock, &readfds))
+                {
+                    recv(telnetSock, telnetBuff, maxLen, 0);
+                    send(serverSock, telnetBuff, strlen(telnetBuff), 0);
+            printf("%s", telnetBuff);
+                }
+                if(FD_ISSET(serverSock, &readfds))
+                {
+                    recv(serverSock, serverBuff, maxLen, 0);
+                    send(telnetSock, serverBuff, strlen(serverBuff), 0);
+            printf("%s", serverBuff);
+                }
+            }
 
-        //Sanitize buffers
-	int i;
-	for(i = 0; i < 1025; i++)
-	{
-	    telnetBuff[i] = '\0';
-	    serverBuff[i] = '\0';
-	}
+            //Sanitize buffers
+        int i;
+        for(i = 0; i < 1025; i++)
+        {
+            telnetBuff[i] = '\0';
+            serverBuff[i] = '\0';
+        }
+    }
 
         /*
 	//Convert input to network-readable language
