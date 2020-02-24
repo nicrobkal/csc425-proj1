d2ef96a7855c2b4b028b221918a6fd2b1f351cbe
nicrobkal <nicrobkal@gmail.com>
2020-02-24 12:01:03 -0700

    Set port to 0

--- sproxy.c
+++ sproxy.c
@@ -108,7 +108,8 @@ int main(int argc, char *argv[])
 
     serverAddr.sin_family = AF_INET; 
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-       
+    serverAddr.sin_port = htonl(0);
+  
     //Bind ip to socket
     if(bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) 
     {
