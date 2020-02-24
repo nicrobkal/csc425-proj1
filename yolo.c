ad631fa81a5ead7670c9933d69a88efb371c5105
nicrobkal <nicrobkal@gmail.com>
2020-02-24 14:20:08 -0700

    Swapped connection with server with telnet connection order

--- yolo.c
+++ yolo.c
@@ -35,32 +35,32 @@ char* removeNewline(char *s)
 
 int main(int argc, char *argv[]) 
 { 
-    int masterSocket = 0, serverSock = 0;
-    int maxLen = 256;
+    int serverSock = 0, telnetSock = 0;
+    int maxLen = 1024;
     int opt = 1; 
-    struct sockaddr_in telnetAddr = {0};
+    struct sockaddr_in telnetAddr;
     int telnetAddrLen = sizeof(telnetAddr);
-    struct sockaddr_in serverAddr = {0};
+    struct sockaddr_in serverAddr;
     fd_set readfds;
     char telnetBuff[1025] = {0};
     char serverBuff[1025] = {0};
 
     //Check if arguments are valid
-    if(argc != 2)
+    if(argc != 4)
     {
         fprintf(stderr, "Arguments invalid. Terminating.\n");
         return 1;
     }
 
     //Create socket file descriptor
-    if ((masterSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    if ((telnetSock = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
         fprintf(stderr, "Socket failed to connect. Terminating.\n");
         return 1;
     } 
        
     //Attach socket to port
-    if (setsockopt(masterSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+    if (setsockopt(telnetSock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
     { 
         fprintf(stderr, "Failed setting sock options. Terminating.\n");
     	return 1;
@@ -68,32 +68,28 @@ int main(int argc, char *argv[])
 
     telnetAddr.sin_family = AF_INET; 
     telnetAddr.sin_addr.s_addr = INADDR_ANY; 
-    telnetAddr.sin_port = htonl(atoi(argv[1])); 
+    telnetAddr.sin_port = htons(atoi(argv[1])); 
        
     //Bind ip to socket
-    if(bind(masterSocket, (struct sockaddr *)&telnetAddr, sizeof(telnetAddr)) < 0) 
+    if(bind(telnetSock, (struct sockaddr *)&telnetAddr, sizeof(telnetAddr)) < 0) 
     {
-        fprintf(stderr, "Server binding failed. Terminating.\n");
+        fprintf(stderr, "Binding failed. Terminating.\n");
         return 1;
     }
     
     //Enable listening on given socket
-    if (listen(masterSocket, 3) < 0)
+    if (listen(telnetSock, 3) < 0) 
     { 
         fprintf(stderr, "Listening failed. Terminating.\n"); 
         exit(EXIT_FAILURE); 
     }
 
-    fprintf(stderr, "Waiting for connection");
-
     //Accept the client
-    if ((masterSocket = accept(masterSocket, (struct sockaddr *)&telnetAddr, (socklen_t*)&telnetAddrLen))<0) 
+    if ((telnetSock = accept(telnetSock, (struct sockaddr *)&telnetAddr, (socklen_t*)&telnetAddrLen))<0) 
     { 
-        fprintf(stderr, "Server accept failed. Terminating.\n");
+        fprintf(stderr, "Accept failed. Terminating.\n");
         return 1;
     }
-
-    fprintf(stderr, "Connected to cproxy!");
 
     //Create initial socket
     if ((serverSock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
@@ -103,11 +99,11 @@ int main(int argc, char *argv[])
     } 
    
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htonl(23); 
+    serverAddr.sin_port = htons(atoi(argv[3])); 
 
     //Bind IP to socket
-    if(inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr) <=0 )  
-    {
+    if(inet_pton(AF_INET, argv[2], &serverAddr.sin_addr) <=0 )  
+    { 
         printf("Invalid Server Address. Terminating.\n"); 
         return 1;
     } 
@@ -119,8 +115,6 @@ int main(int argc, char *argv[])
         return 1; 
     }
 
-    fprintf(stderr, "Connected to telnet!");
-
     //While user is still inputting data
     while(1)
     {
@@ -129,13 +123,13 @@ int main(int argc, char *argv[])
         FD_ZERO(&readfds);
 
         //Add descriptors
-        FD_SET(masterSocket, &readfds);
+        FD_SET(telnetSock, &readfds);
         FD_SET(serverSock, &readfds);
 
         //Find larger file descriptor
-        if(masterSocket > serverSock)
+        if(telnetSock > serverSock)
         {
-            n = masterSocket + 1;
+            n = telnetSock + 1;
         }
         else
         {
@@ -147,77 +141,76 @@ int main(int argc, char *argv[])
 
         if (rv == -1)
         {
-            fprintf(stderr, "Select() function failed.\n");
-            close(masterSocket);
-            close(serverSock);
-            return 1;
+            fprintf(stderr, "Select() function failed.");
+	    close(telnetSock);
+	    close(serverSock);
+	    return 1;
         }
         else if(rv == 0)
         {
-            printf("Timeout occurred! No data after 10.5 seconds.\n");
-            close(masterSocket);
-            close(serverSock);
-            return 1;
+            printf("Timeout occurred! No data after 10.5 seconds.");
+            close(telnetSock);
+	    close(serverSock);
+	    return 1;
         }
         else
         {
             //One or both descrptors have data
-            if(FD_ISSET(masterSocket, &readfds))
+            if(FD_ISSET(telnetSock, &readfds))
             {
-                recv(masterSocket, telnetBuff, maxLen, 0);
+                recv(telnetSock, telnetBuff, maxLen, 0);
                 send(serverSock, telnetBuff, strlen(telnetBuff), 0);
-                printf("%s", telnetBuff);
+		printf("%s", telnetBuff);
             }
             if(FD_ISSET(serverSock, &readfds))
             {
                 recv(serverSock, serverBuff, maxLen, 0);
-                send(masterSocket, serverBuff, strlen(serverBuff), 0);
-                printf("%s", serverBuff);
+                send(telnetSock, serverBuff, strlen(serverBuff), 0);
+		printf("%s", serverBuff);
             }
         }
-
-            //Sanitize buffers
-        int i;
-        for(i = 0; i < 1025; i++)
+	
+	//Sanitize buffers
+	int i;
+	for(i = 0; i < 1025; i++)
+	{
+	    telnetBuff[i] = '\0';
+	    serverBuff[i] = '\0';
+	}
+        /*
+	//Convert input to network-readable language
+        uint32_t temp = htonl(strlen(removeNewline(telnetBuff)));
+        
+        //Checks if string is valid
+        if(strlen(removeNewline(telnetBuff)) > 0)
         {
-            telnetBuff[i] = '\0';
-            serverBuff[i] = '\0';
-        }
+            //Send the first packet holding the size of the coming message in bytes
+            send(serverSock, &temp, 4, 0);
+
+            //Sanitize input
+            char* newBuff = removeNewline(telnetBuff);
+
+            //Send the actual message
+            send(serverSock, newBuff, strlen(newBuff), 0); 
+
+            //Check if packet was valid
+            if(valRead < 0)
+            {
+                fprintf(stderr, "Failed to read from sock. Terminating.\n");
+                return 1;
+            }
+
+            //Sanitizr telnetBuff
+            int i;
+            for(i = 0; i < 1025; i++)
+            {
+                telnetBuff[i] = '\0';
+            }
+        }*/
     }
 
-    /*
-	//Convert input to network-readable language
-    uint32_t temp = htonl(strlen(removeNewline(telnetBuff)));
-    
-    //Checks if string is valid
-    if(strlen(removeNewline(telnetBuff)) > 0)
-    {
-        //Send the first packet holding the size of the coming message in bytes
-        send(serverSock, &temp, 4, 0);
-
-        //Sanitize input
-        char* newBuff = removeNewline(telnetBuff);
-
-        //Send the actual message
-        send(serverSock, newBuff, strlen(newBuff), 0); 
-
-        //Check if packet was valid
-        if(valRead < 0)
-        {
-            fprintf(stderr, "Failed to read from sock. Terminating.\n");
-            return 1;
-        }
-
-        //Sanitizr telnetBuff
-        int i;
-        for(i = 0; i < 1025; i++)
-        {
-            telnetBuff[i] = '\0';
-        }
-    }*/
-
     //Close the sockets
-    close(masterSocket);
+    close(telnetSock);
     close(serverSock);
 
     return 0; 
