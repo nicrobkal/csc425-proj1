92e850fa44c1e3d19bb837c006c0d5599d09f3ae
Nick Kalkanian <nicrobkal@gmail.com>
2020-02-24 08:48:33 -0700

    Maybe fix

--- sproxy.c
+++ sproxy.c
@@ -107,8 +107,7 @@ int main(int argc, char *argv[])
     }
 
     serverAddr.sin_family = AF_INET; 
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    serverAddr.sin_port = htons(23);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
        
     //Bind ip to socket
     if(bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) 
