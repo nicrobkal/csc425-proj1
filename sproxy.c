e9f3ce24fee720962144b2c8e740e36d7939587a
nicrobkal <nicrobkal@gmail.com>
2020-02-24 12:50:24 -0700

    PORT 22

--- sproxy.c
+++ sproxy.c
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
 
     serverAddr.sin_family = AF_INET; 
     serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htonl(23);
+    serverAddr.sin_port = htonl(22);
   
     //Bind ip to socket
     if (bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) 
